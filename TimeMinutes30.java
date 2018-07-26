9ae1975d2cfa031e827b1fae461232074265e260
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-07-26 19:33:10 +0530

    Create TimeMinutes30.java

---
+++ TimeMinutes30.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class TimeMinutes30 {
+	public static void main(String[] args) {
+	 Scanner in=new Scanner(System.in);	
+	 float h=in.nextFloat();
+	 float h1=in.nextFloat();
+	 float h2=Math.abs(h-h1);
+	 System.out.println(h2);	
+	}
+}
