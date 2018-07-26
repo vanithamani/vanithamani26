f96949f1506c56e235c09ced47faa1832fd3b536
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-07-26 17:57:42 +0530

    Update Numeric.java

--- Numeric.java
+++ Numeric.java
@@ -3,7 +3,7 @@ public class Numeric {
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
 		String n=in.nextLine();
-		String n1="[0-9]+.[0-9+";
+		String n1="[0-9]+.[0-9]+";
 		if(n.matches(n1)) {
 		System.out.println("yes");
 	}else
