2adaf391077419954fbc0edcd8baa7696f65471f
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-07-26 17:51:23 +0530

    Create Numeric.java

---
+++ Numeric.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Numeric {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String n=in.nextLine();
+		String n1="[0-9]+";
+		if(n.matches(n1)) {
+		System.out.println("yes");
+	}else
+		System.out.println("no");
+}
+}
