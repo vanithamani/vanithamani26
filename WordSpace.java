3cc526d191b9dc21e9d133679b8e8a5a9f22ac11
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-07-27 13:33:04 +0530

    Create WordSpace.java

---
+++ WordSpace.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class WordSpace {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String s=in.nextLine();
+		int sum=0;
+		String s1[]=s.split("\\s");
+		int n=s1.length;
+		 sum=sum+n;	
+		System.out.println(sum-1);
+	}
+
+}
