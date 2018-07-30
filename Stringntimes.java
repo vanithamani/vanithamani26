4fcf7be5187e7be8d4a688dd20a21e9f357ec90d
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-07-30 17:30:49 +0530

    Create Stringntimes.java

---
+++ Stringntimes.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Stringntimes {
+	public static void main(String[] args) {
+	 Scanner in=new Scanner(System.in);
+	 String s1=in.nextLine();
+	 int num=in.nextInt();
+	for(int i=1;i<=num;i++)
+	{
+		System.out.println(s1);
+	}
+	}
+}
