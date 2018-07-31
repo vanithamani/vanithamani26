b96e8a433d1da25a17c0ccac4f7f1cb2e2e700af
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-07-31 14:34:37 +0530

    Create Add1value.java

---
+++ Add1value.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Add1value {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+				int n=in.nextInt();
+				System.out.print(n+1);
+	}
+}
