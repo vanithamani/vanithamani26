be81f33a9fb7389e4959a854bff86d887724bbf9
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-07-31 15:17:44 +0530

    Create Averageofnum.java

---
+++ Averageofnum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Averageofnum {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         int a[]=new int[n];
+         int sum=0;
+         for(int i=0;i<n;i++)
+         {
+        	 a[i]=in.nextInt();
+         }
+         for(int j=0;j<n;j++)
+         {
+        	 sum=sum+a[j];
+         }
+         System.out.print(sum/n);
+	}
+}
