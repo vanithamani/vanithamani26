c2e3c540c12dacdca43b8231682c94443639ff53
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-07-31 15:26:12 +0530

    Create sum0f2num.java

---
+++ sum0f2num.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class sum0f2num {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int n1=in.nextInt();
+		System.out.println(n+n1);
+	}
+}
