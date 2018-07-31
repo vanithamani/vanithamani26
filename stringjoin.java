a10ededd4e791161b8485d92e575cdcd677fe1cc
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-07-31 13:21:27 +0530

    Create stringjoin.java

---
+++ stringjoin.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+public class stringjoin {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String s1=in.nextLine();
+		String s2=in.nextLine();
+		System.out.println(s1+s2);
+	}
+}
