28b1a377cc6eeab3ac89a523a070920c209ea735
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-08-01 15:48:59 +0530

    Create numsameOrder.java

---
+++ numsameOrder.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class numsameOrder {
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	String s=Integer.toString(n);
+	for(int i=0;i<s.length();i++)
+	System.out.print(" "+s.charAt(i));
+	}
+}
