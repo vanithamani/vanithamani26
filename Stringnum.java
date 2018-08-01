c3e07b450c64267ea96fa8ab73f782b7e33d9423
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-08-01 17:40:34 +0530

    Create Stringnum.java

---
+++ Stringnum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Stringnum {
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String s=in.nextLine();
+int n=in.nextInt();
+String s1=s.substring(0, n);
+System.out.println(s1);
+	}
+}
