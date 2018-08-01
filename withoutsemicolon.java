331e4dcc103d8932de28a6af8e341e0ad9bb5186
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-08-01 19:00:37 +0530

    Create withoutsemicolon.java

---
+++ withoutsemicolon.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+public class withoutsemicolon {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		if(System.out.printf(in.nextLine())==null)
+	    in.close();
+	}
+}
