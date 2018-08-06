cfbecafcc17099c674b9aff3a7fa543a932dcdad
vanithamani <41186650+vanithamani@users.noreply.github.com>
2018-08-06 17:20:14 +0530

    Create Ascii_value.java

---
+++ Ascii_value.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Ascii_value {
+	public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	char ch=in.next().charAt(0);
+     int asci=ch;
+		System.out.print(asci);
+	}
+}
